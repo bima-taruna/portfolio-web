16b9849b18ece13b564287109d530a409b700610
bima-taruna <bimataruna21@gmail.com>
2024-07-31 21:58:02 +0700

    change packages

--- src/store.tsx
+++ src/store.tsx
@@ -1,13 +1,7 @@
-import { applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-import authReducer from "./reducers/auth";
-import message from "./reducers/message";
-
-const middleware = [thunk];
 
 const store = configureStore({
   reducer: rootReducer,
@@ -15,7 +9,6 @@ const store = configureStore({
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
